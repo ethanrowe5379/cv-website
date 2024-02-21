3a6e60e4e2657a2821428b72b4279d0c1b7bfa13
Ethan Rowe <ethanrowe5379@gmail.com>
2024-02-21 19:42:52 +0000

    navigation molecule

---
+++ Components/molecules/nav.tsx
@@ -0,0 +1,28 @@
+'use client';
+
+//import Link from "../atoms/link";
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import clsx from 'clsx';
+
+//Map of links of website
+const links = [
+    {key: 'home', name: 'Home', href: '/home'},
+    {key:'about', name: 'About', href: '/home/about'},
+    {key:'projects', name: 'Projects', href: '/home/projects'},
+    {key:'contact', name: 'Contact', href: '/home/contact'}
+];
+
+export default function Nav(){
+    const pathname = usePathname();
+    return(
+        <nav className="">
+            {links.map((link) => {
+                return (
+                    <Link className="font-medium text-slate-500 hover:text-blue-600 mr-5" key={link.key} href={link.href} ><p className="inline-block">{link.name}</p></Link>
+                );
+            }
+            )}
+        </nav>
+    );
+}
