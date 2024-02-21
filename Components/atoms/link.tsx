85b05a340805103b5473110b3673429664c8fe11
Ethan Rowe <ethanrowe5379@gmail.com>
2024-02-21 19:41:40 +0000

    link atom

---
+++ Components/atoms/link.tsx
@@ -0,0 +1,8 @@
+import Link from 'next/link'
+import clsx from 'clsx'
+
+// export default function link({href, content} : {href: string, content: string}){
+//     return (
+//         <Link href={href}>{content}</Link>
+//     );
+// }
