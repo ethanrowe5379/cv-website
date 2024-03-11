eeb75802ed6a534fba934309224ef67826cbc194
Ethan Rowe <ethanrowe5379@gmail.com>
2024-03-11 23:25:35 +0000

    removed url class and a tage from card molecule

--- Components/molecules/card.tsx
+++ Components/molecules/card.tsx
@@ -4,14 +4,13 @@ import { cardData } from '@/app/lib/definitions';
 import SubTitle from "../atoms/subsectiontitle";
 import Link from "next/link";
 
-export default function card({title, text, url}: {title: string, text: string, url: string}){
-    console.log(url)
+export default function card({title, text}: {title: string, text: string}){
+
     return (
-        <a href={url}>
         <section className="bg-slate-200 hover:bg-slate-100">
             <SubTitle content={title} css=""/>
             <p className="text-base">{text}</p>
         </section>
-        </a>
+
     );
 }
