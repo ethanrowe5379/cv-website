bf712653805e9484b3f826219e5a81fac1f419df
Ethan Rowe <ethanrowe5379@gmail.com>
2024-03-11 23:16:39 +0000

    Card Molecule

--- Components/molecules/card.tsx
+++ Components/molecules/card.tsx
@@ -1,10 +1,17 @@
-import Title from "../atoms/Title";
+'use client';
+import { fetchContent } from "@/app/lib/Home/fetchContent";
+import { cardData } from '@/app/lib/definitions';
+import SubTitle from "../atoms/subsectiontitle";
+import Link from "next/link";
 
-export default function card({titleContent, text} : {titleContent: string, text: string}){
+export default function card({title, text, url}: {title: string, text: string, url: string}){
+    console.log(url)
     return (
-        <section className="bg-slate-200">
-            <Title content={titleContent}/>
-            <p>{text}</p>
+        <a href={url}>
+        <section className="bg-slate-200 hover:bg-slate-100">
+            <SubTitle content={title}/>
+            <p className="text-base">{text}</p>
         </section>
+        </a>
     );
 }
