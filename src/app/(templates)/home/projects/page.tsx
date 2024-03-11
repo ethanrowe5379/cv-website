faa1e9e85884ffcafdb3436188482d31175a1d3d
Ethan Rowe <ethanrowe5379@gmail.com>
2024-03-11 23:17:13 +0000

    project page template

--- src/app/(templates)/home/projects/page.tsx
+++ src/app/(templates)/home/projects/page.tsx
@@ -1,10 +1,33 @@
+'use server';
 
+import { fetchProjects } from "@/app/lib/Projects/fetchProjects";
+import { Octokit } from "octokit";
+import GitCard from "../../../../../Components/molecules/GitCard";
+import Title from "../../../../../Components/atoms/Title";
 
-export default function Page(){
+export default async function Page(){
+    const projects = await fetchProjects();
     return (
-        <main>
-            <h2>Github</h2>
-            <p></p>
-        </main>
+        <>
+            <main>
+                <Title content="Github"/>
+                <ol>
+                {
+                    projects.map((items, index) => {
+                        if(items.description == null){
+                            items.description = "Description Unavailable - View Readme.md in on github";
+                        }
+                        if(items.updated_at == null || items.updated_at == undefined){
+                            items.updated_at = "Time Unavailable";
+                        }
+                        return (
+                            <GitCard key={index} title={items.name} time={items.updated_at} text={items.description} url={items.html_url}/>
+                        )
+                    })
+                }
+                </ol>
+                <p>Created using the Github API via Octokit</p>
+            </main>
+        </>
     );
 };
